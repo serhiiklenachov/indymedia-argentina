cfad328beb97fb67f178bcfbec760769ee8c081e
Roberto Vaccaro <roberto.vaccaro@gmail.com>
2018-07-13 19:24:35 -0300

    Corregido doble inclusión de script

--- inc/customizer_editor.php
+++ inc/customizer_editor.php
@@ -1,7 +1,6 @@
 <?php
 
-class WP_Customize_Editor extends WP_Customize_Control
-{
+class WP_Customize_Editor extends WP_Customize_Control {
 	public $type = 'editor';
 
 	public function render_content() { ?>
@@ -21,9 +20,4 @@ class WP_Customize_Editor extends WP_Customize_Control
 		add_filter( 'the_editor', function( $output ) { return preg_replace( '/<textarea/', '<textarea ' . $this->get_link(), $output, 1 ); } );
 	}
 }
-
-function editor_customizer_script() {
-	wp_enqueue_script( 'wp-editor-customizer', get_template_directory_uri() . '/js/customizer-panel.js', array( 'jquery' ), rand(), true );
-}
-add_action( 'customize_controls_enqueue_scripts', 'editor_customizer_script' );
 
