30a81863a6e467ef04f16196e340dd159915384e
Roberto Vaccaro <roberto.vaccaro@gmail.com>
2018-08-09 12:30:11 -0300

    Arreglado error de paginación

--- category.php
+++ category.php
@@ -2,19 +2,20 @@
 <div class="vl-container">
 <?php
 	if ( $wp_query->is_paged ) {
-		set_query_var( 'offset', min( intval( $_GET['offset'] ), 25 ) );
-		get_template_part('template-parts/category', 'archive');
+		get_template_part( 'template-parts/category', 'archive' );
 	} else {
-		get_template_part('template-parts/category', 'main');
+		get_template_part( 'template-parts/category', 'main' );
 		
-		$wp_query->post_count = count(WP_Deduplicator::get());
+		$wp_query->post_count = count( WP_Deduplicator::get() );
+		$to_show = $wp_query->found_posts - $wp_query->post_count;
+		$wp_query->max_num_pages = absint( ceil( $to_show / $posts_per_page + 1 ) );
+
 		the_posts_pagination( array(
 			'add_args' => array(
-				'offset' => count(WP_Deduplicator::get())
+				'offset' => count( $wp_query->post_count )
 			) )
 		);
 	}
 ?>
 </div>
-<?php //var_dump($wp_query); ?>
 <?php get_footer(); ?>
