fbd260406164fb97b61c818794bbdb16419500b9
Roberto Vaccaro <roberto.vaccaro@gmail.com>
2018-08-09 14:31:40 -0300

    Nuevo editor de contenido para el customizer

--- inc/customizer_editor.php
+++ inc/customizer_editor.php
@@ -6,17 +6,31 @@ class WP_Customize_Editor extends WP_Customize_Control {
 	public function render_content() { ?>
 		<label>
 			<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
+			<input id="<?php echo $this->id; ?>-link" type="hidden" class="wp-editor-area" <?php $this->link(); ?> value="<?php echo esc_textarea( $this->value() ); ?>">
 			<?php
-			$this->filter_editor_setting_link();
-			wp_editor($this->value(), $this->id, array() );
+			$settings = array(
+				'textarea_name' => $this->id,
+				'media_buttons' => true,
+				'tinymce' => array(
+					'setup' => "function (editor) {
+						var cb = function () {
+							var linkInput = document.getElementById('$this->id-link')
+							linkInput.value = editor.getContent()
+							linkInput.dispatchEvent(new Event('change'))
+						}
+						editor.on('Change', cb)
+						editor.on('Undo', cb)
+						editor.on('Redo', cb)
+						editor.on('KeyUp', cb) // Remove this if it seems like an overkill
+					}"
+				)
+			);
+			wp_editor($this->value(), $this->id, $settings );
+
+			do_action('admin_footer');
+			do_action('admin_print_footer_scripts');
 		?>
 		</label>
 	<?php
-		do_action('admin_footer');
-		do_action('admin_print_footer_scripts');
-	}
-
-	private function filter_editor_setting_link() {
-		add_filter( 'the_editor', function( $output ) { return preg_replace( '/<textarea/', '<textarea ' . $this->get_link(), $output, 1 ); } );
 	}
 }
