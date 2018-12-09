06e38beccc9ff7c120ff4ce0bf0a22e5e7b267f4
Roberto Vaccaro <roberto.vaccaro@gmail.com>
2018-12-09 12:27:17 -0300

    Customizer Migration: Fuentes

--- inc/customizer.php
+++ inc/customizer.php
@@ -15,6 +15,8 @@ function indymedia_customize_register( $wp_customize ) {
 		)
 	);
 
+	$defaultFont = 'Roboto:400,400italic,700,700italic';
+
 	$wp_customize->add_setting(
 		'indymedia_background_header_images',
 		array(
@@ -27,7 +29,7 @@ function indymedia_customize_register( $wp_customize ) {
 	$wp_customize->add_setting(
 		'indymedia_section_tag_font',
 		array(
-			'default'			=> 'Roboto:400,400italic,700,700italic',
+			'default'			=> $defaultFont,
 			'capability'		=> 'edit_theme_options',
 			'transport'         => 'postMessage',
 		)
@@ -36,7 +38,7 @@ function indymedia_customize_register( $wp_customize ) {
 	$wp_customize->add_setting(
 		'indymedia_hero_title_font',
 		array(
-			'default'			=> 'Roboto:400,400italic,700,700italic',
+			'default'			=> $defaultFont,
 			'capability'		=> 'edit_theme_options',
 			'transport'         => 'postMessage',
 		)
@@ -45,7 +47,7 @@ function indymedia_customize_register( $wp_customize ) {
 	$wp_customize->add_setting(
 		'indymedia_block_title_font',
 		array(
-			'default'			=> 'Roboto:400,400italic,700,700italic',
+			'default'			=> $defaultFont,
 			'capability'		=> 'edit_theme_options',
 			'transport'         => 'postMessage',
 		)
@@ -54,7 +56,7 @@ function indymedia_customize_register( $wp_customize ) {
 	$wp_customize->add_setting(
 		'indymedia_post_title_font',
 		array(
-			'default'			=> 'Roboto:400,400italic,700,700italic',
+			'default'			=> $defaultFont,
 			'capability'		=> 'edit_theme_options',
 			'transport'         => 'postMessage',
 		)
@@ -63,7 +65,7 @@ function indymedia_customize_register( $wp_customize ) {
 	$wp_customize->add_setting(
 		'indymedia_post_excerpt_font',
 		array(
-			'default'			=> 'Roboto:400,400italic,700,700italic',
+			'default'			=> $defaultFont,
 			'capability'		=> 'edit_theme_options',
 			'transport'         => 'postMessage',
 		)
@@ -72,7 +74,7 @@ function indymedia_customize_register( $wp_customize ) {
 	$wp_customize->add_setting(
 		'indymedia_widget_title_font',
 		array(
-			'default'			=> 'Roboto:400,400italic,700,700italic',
+			'default'			=> $defaultFont,
 			'capability'		=> 'edit_theme_options',
 			'transport'         => 'postMessage',
 		)
@@ -81,7 +83,7 @@ function indymedia_customize_register( $wp_customize ) {
 	$wp_customize->add_setting(
 		'indymedia_entry_title_font',
 		array(
-			'default'			=> 'Roboto:400,400italic,700,700italic',
+			'default'			=> $defaultFont,
 			'capability'		=> 'edit_theme_options',
 			'transport'         => 'postMessage',
 		)
@@ -90,7 +92,7 @@ function indymedia_customize_register( $wp_customize ) {
 	$wp_customize->add_setting(
 		'indymedia_entry_content_font',
 		array(
-			'default'			=> 'Roboto:400,400italic,700,700italic',
+			'default'			=> $defaultFont,
 			'capability'		=> 'edit_theme_options',
 			'transport'         => 'postMessage',
 		)
@@ -99,7 +101,7 @@ function indymedia_customize_register( $wp_customize ) {
 	$wp_customize->add_setting(
 		'indymedia_top_header_font',
 		array(
-			'default'			=> 'Roboto:400,400italic,700,700italic',
+			'default'			=> $defaultFont,
 			'capability'		=> 'edit_theme_options',
 			'transport'         => 'postMessage',
 		)
@@ -108,7 +110,7 @@ function indymedia_customize_register( $wp_customize ) {
 	$wp_customize->add_setting(
 		'indymedia_main_navigation_font',
 		array(
-			'default'			=> 'Roboto:400,400italic,700,700italic',
+			'default'			=> $defaultFont,
 			'capability'		=> 'edit_theme_options',
 			'transport'         => 'postMessage',
 		)
@@ -117,7 +119,7 @@ function indymedia_customize_register( $wp_customize ) {
 	$wp_customize->add_setting(
 		'indymedia_ticker_font',
 		array(
-			'default'			=> 'Roboto:400,400italic,700,700italic',
+			'default'			=> $defaultFont,
 			'capability'		=> 'edit_theme_options',
 			'transport'         => 'postMessage',
 		)
@@ -141,19 +143,6 @@ function indymedia_customize_register( $wp_customize ) {
 	);
 
 	/**
-	 * Sections
-	 */
-
-	$wp_customize->add_section(
-		'indymedia_fonts',
-		array(
-			'title'				=> 'Fuentes',
-			'panel'				=> 'viral_general_settings_panel',
-			'priority'			=> 50
-		)
-	);
-
-	/**
 	 * Controls
 	 */
 	$wp_customize->add_control( 
@@ -165,203 +154,6 @@ function indymedia_customize_register( $wp_customize ) {
 				'section'     => 'colors',
 				'description' => 'Los colores que se muestran al pasar el mouse por encima de un objeto requieren recargar la página para actualizarlos',
 				'settings'    => 'indymedia_main_color',
-			)
-		)
-	);
-
-	$font_choices = array(
-		'Arimo:400,700,400italic,700italic' => 'Arimo',
-		'Arvo:400,700,400italic,700italic' => 'Arvo',
-		'Bitter:400,700,400italic' => 'Bitter',
-		'Cabin:400,700,400italic' => 'Cabin',
-		'Droid Sans:400,700' => 'Droid Sans',
-		'Droid Serif:400,700,400italic,700italic' => 'Droid Serif',
-		'Fjalla One:400' => 'Fjalla One',
-		'Francois One:400' => 'Francois One',
-		'Josefin Sans:400,300,600,700' => 'Josefin Sans',
-		'Lato:400,700,400italic,700italic' => 'Lato',
-		'Libre Baskerville:400,400italic,700' => 'Libre Baskerville',
-		'Lora:400,700,400italic,700italic' => 'Lora',
-		'Merriweather:400,300italic,300,400italic,700,700italic' => 'Merriweather',
-		'Montserrat:400,700' => 'Montserrat',
-		'Open Sans:400italic,700italic,400,700' => 'Open Sans',
-		'Open Sans Condensed:700,300italic,300' => 'Open Sans Condensed',
-		'Oswald:400,700' => 'Oswald',
-		'Oxygen:400,300,700' => 'Oxygen',
-		'Playfair Display:400,700,400italic' => 'Playfair Display',
-		'PT Sans:400,700,400italic,700italic' => 'PT Sans',
-		'PT Sans Narrow:400,700' => 'PT Sans Narrow',
-		'PT Serif:400,700' => 'PT Serif',
-		'Raleway:400,700' => 'Raleway',
-		'Roboto:400,400italic,700,700italic' => 'Roboto',
-		'Roboto Condensed:400italic,700italic,400,700' => 'Roboto Condensed',
-		'Roboto Slab:400,700' => 'Roboto Slab',
-		'Rokkitt:400' => 'Rokkitt',
-		'Source Sans Pro:400,700,400italic,700italic' => 'Source Sans Pro',
-		'Ubuntu:400,700,400italic,700italic' => 'Ubuntu',
-		'Yanone Kaffeesatz:400,700' => 'Yanone Kaffeesatz',
-	);
-
-	$wp_customize->add_control( 
-		new \WP_Customize_Control(
-			$wp_customize,
-			'indymedia_section_tag_font_selector',
-			array(
-				'label'       => __( 'Etiquetas de Sección', 'indymedia' ),
-				'type'        => 'select',
-				'section'     => 'indymedia_fonts',
-				'settings'    => 'indymedia_section_tag_font',
-				'choices'     => $font_choices
-			)
-		)
-	);
-
-	$wp_customize->add_control( 
-		new \WP_Customize_Control(
-			$wp_customize,
-			'indymedia_hero_title_font_selector',
-			array(
-				'label'       => __( 'Títulos Destacados', 'indymedia' ),
-				'description' => __( 'Fuente de los títulos de las noticias destacadas mostradas en la parte superior de la primera plana', 'indymedia' ),
-				'type'        => 'select',
-				'section'     => 'indymedia_fonts',
-				'settings'    => 'indymedia_hero_title_font',
-				'choices'     => $font_choices
-			)
-		)
-	);
-
-	$wp_customize->add_control( 
-		new \WP_Customize_Control(
-			$wp_customize,
-			'indymedia_block_title_font_selector',
-			array(
-				'label'       => __( 'Títulos de Bloque', 'indymedia' ),
-				'description' => __( 'Fuente de los títulos de los bloques mostrados en primera plana', 'indymedia' ),
-				'type'        => 'select',
-				'section'     => 'indymedia_fonts',
-				'settings'    => 'indymedia_block_title_font',
-				'choices'     => $font_choices
-			)
-		)
-	);
-
-	$wp_customize->add_control( 
-		new \WP_Customize_Control(
-			$wp_customize,
-			'indymedia_post_title_font_selector',
-			array(
-				'label'       => __( 'Títulos de Artículos', 'indymedia' ),
-				'description' => __( 'Fuente de los títulos de artículos mostrados en primera plana', 'indymedia' ),
-				'type'        => 'select',
-				'section'     => 'indymedia_fonts',
-				'settings'    => 'indymedia_post_title_font',
-				'choices'     => $font_choices
-			)
-		)
-	);
-
-	$wp_customize->add_control( 
-		new \WP_Customize_Control(
-			$wp_customize,
-			'indymedia_post_excerpt_font_selector',
-			array(
-				'label'       => __( 'Extractos de Artículos', 'indymedia' ),
-				'description' => __( 'Fuente de los resumenes de artículos mostrados en primera plana', 'indymedia' ),
-				'type'        => 'select',
-				'section'     => 'indymedia_fonts',
-				'settings'    => 'indymedia_post_excerpt_font',
-				'choices'     => $font_choices
-			)
-		)
-	);
-
-	$wp_customize->add_control( 
-		new \WP_Customize_Control(
-			$wp_customize,
-			'indymedia_widget_title_font_selector',
-			array(
-				'label'       => __( 'Títulos de Widgets', 'indymedia' ),
-				'description' => __( 'Fuente de los títulos de los widgets', 'indymedia' ),
-				'type'        => 'select',
-				'section'     => 'indymedia_fonts',
-				'settings'    => 'indymedia_widget_title_font',
-				'choices'     => $font_choices
-			)
-		)
-	);
-
-	$wp_customize->add_control( 
-		new \WP_Customize_Control(
-			$wp_customize,
-			'indymedia_entry_title_font_selector',
-			array(
-				'label'       => __( 'Títulos de las Entradas', 'indymedia' ),
-				'description' => __( 'Fuente de los títulos de las entradas individuales', 'indymedia' ),
-				'type'        => 'select',
-				'section'     => 'indymedia_fonts',
-				'settings'    => 'indymedia_entry_title_font',
-				'choices'     => $font_choices
-			)
-		)
-	);
-
-	$wp_customize->add_control( 
-		new \WP_Customize_Control(
-			$wp_customize,
-			'indymedia_entry_content_font_selector',
-			array(
-				'label'       => __( 'Contenido de las Entradas', 'indymedia' ),
-				'description' => __( 'Fuente del contenido de las entradas individuales', 'indymedia' ),
-				'type'        => 'select',
-				'section'     => 'indymedia_fonts',
-				'settings'    => 'indymedia_entry_content_font',
-				'choices'     => $font_choices
-			)
-		)
-	);
-
-	$wp_customize->add_control( 
-		new \WP_Customize_Control(
-			$wp_customize,
-			'indymedia_top_header_font_selector',
-			array(
-				'label'       => __( 'Menú Superior de la Cabecera', 'indymedia' ),
-				'description' => __( 'Fuente del menú ubicado en la parte superior de la página', 'indymedia' ),
-				'type'        => 'select',
-				'section'     => 'indymedia_fonts',
-				'settings'    => 'indymedia_top_header_font',
-				'choices'     => $font_choices
-			)
-		)
-	);
-
-	$wp_customize->add_control( 
-		new \WP_Customize_Control(
-			$wp_customize,
-			'indymedia_main_navigation_font_selector',
-			array(
-				'label'       => __( 'Menú Principal', 'indymedia' ),
-				'description' => __( 'Fuente del menú principal del sitio', 'indymedia' ),
-				'type'        => 'select',
-				'section'     => 'indymedia_fonts',
-				'settings'    => 'indymedia_main_navigation_font',
-				'choices'     => $font_choices
-			)
-		)
-	);
-
-	$wp_customize->add_control( 
-		new \WP_Customize_Control(
-			$wp_customize,
-			'indymedia_ticker_font_selector',
-			array(
-				'label'       => __( 'Teletipo', 'indymedia' ),
-				'description' => __( 'Fuente del teletipo, tanto título como contenido.', 'indymedia' ),
-				'type'        => 'select',
-				'section'     => 'indymedia_fonts',
-				'settings'    => 'indymedia_ticker_font',
-				'choices'     => $font_choices
 			)
 		)
 	);
@@ -427,6 +219,71 @@ function indymedia_customize_preview() {
 add_action( 'customize_preview_init', 'indymedia_customize_preview' );
 
 function indymedia_customizer_controls() {
+	wp_register_script( 'indymedia_customizer', get_stylesheet_directory_uri() . '/js/customizer.js', array(), null, true );
+
+	$data = array();
+
+	$data[ 'fonts' ] = array(
+		'Arimo:400,700,400italic,700italic' => 'Arimo',
+		'Arvo:400,700,400italic,700italic' => 'Arvo',
+		'Bitter:400,700,400italic' => 'Bitter',
+		'Cabin:400,700,400italic' => 'Cabin',
+		'Droid Sans:400,700' => 'Droid Sans',
+		'Droid Serif:400,700,400italic,700italic' => 'Droid Serif',
+		'Fjalla One:400' => 'Fjalla One',
+		'Francois One:400' => 'Francois One',
+		'Josefin Sans:400,300,600,700' => 'Josefin Sans',
+		'Lato:400,700,400italic,700italic' => 'Lato',
+		'Libre Baskerville:400,400italic,700' => 'Libre Baskerville',
+		'Lora:400,700,400italic,700italic' => 'Lora',
+		'Merriweather:400,300italic,300,400italic,700,700italic' => 'Merriweather',
+		'Montserrat:400,700' => 'Montserrat',
+		'Open Sans:400italic,700italic,400,700' => 'Open Sans',
+		'Open Sans Condensed:700,300italic,300' => 'Open Sans Condensed',
+		'Oswald:400,700' => 'Oswald',
+		'Oxygen:400,300,700' => 'Oxygen',
+		'Playfair Display:400,700,400italic' => 'Playfair Display',
+		'PT Sans:400,700,400italic,700italic' => 'PT Sans',
+		'PT Sans Narrow:400,700' => 'PT Sans Narrow',
+		'PT Serif:400,700' => 'PT Serif',
+		'Raleway:400,700' => 'Raleway',
+		'Roboto:400,400italic,700,700italic' => 'Roboto',
+		'Roboto Condensed:400italic,700italic,400,700' => 'Roboto Condensed',
+		'Roboto Slab:400,700' => 'Roboto Slab',
+		'Rokkitt:400' => 'Rokkitt',
+		'Source Sans Pro:400,700,400italic,700italic' => 'Source Sans Pro',
+		'Ubuntu:400,700,400italic,700italic' => 'Ubuntu',
+		'Yanone Kaffeesatz:400,700' => 'Yanone Kaffeesatz'
+	);
+
+	$data[ 'l10n' ] = array(
+		'fontsSectionTitle' => __( 'Fuentes', 'indymedia' ),
+		'sectionTagsFont' => __( 'Etiquetas de Sección', 'indymedia' ),
+		'heroTitleFont' => __( 'Títulos Destacados', 'indymedia' ),
+		'heroTitleFontDescription' => __( 'Fuente de los títulos de las noticias destacadas mostradas en la parte superior de la primera plana', 'indymedia' ),
+		'blockTitleFont' => __( 'Títulos de Bloque', 'indymedia' ),
+		'blockTitleFontDescription' => __( 'Fuente de los títulos de los bloques mostrados en primera plana', 'indymedia' ),
+		'postTitleFont' => __( 'Títulos de Artículos', 'indymedia' ),
+		'postTitleFontDescription' => __( 'Fuente de los títulos de artículos mostrados en primera plana', 'indymedia' ),
+		'postExcerptFont' => __( 'Extractos de Artículos', 'indymedia' ),
+		'postExcerptFontDescription' => __( 'Fuente de los resumenes de artículos mostrados en primera plana', 'indymedia' ),
+		'widgetTitleFont' => __( 'Títulos de Widgets', 'indymedia' ),
+		'widgetTitleFontDescription' => __( 'Fuente de los títulos de los widgets', 'indymedia' ),
+		'entryTitleFont' => __( 'Títulos de las Entradas', 'indymedia' ),
+		'entryTitleFontDescription' => __( 'Fuente de los títulos de las entradas individuales', 'indymedia' ),
+		'entryContentFont' => __( 'Contenido de las Entradas', 'indymedia' ),
+		'entryContentFontDescription' => __( 'Fuente del contenido de las entradas individuales', 'indymedia' ),
+		'topHeaderFont' => __( 'Menú Superior de la Cabecera', 'indymedia' ),
+		'topHeaderFontDescription' => __( 'Fuente del menú ubicado en la parte superior de la página', 'indymedia' ),
+		'mainNavigationFont' => __( 'Menú Principal', 'indymedia' ),
+		'mainNavigationFontDescription' => __( 'Fuente del menú principal del sitio', 'indymedia' ),
+		'tickerFont' => __( 'Teletipo', 'indymedia' ),
+		'tickerFontDescription' => __( 'Fuente del teletipo, tanto título como contenido.', 'indymedia' )
+	);
+
+	wp_localize_script( 'indymedia_customizer', '_wpIndymediaData', $data );
+	wp_enqueue_script( 'indymedia_customizer' );
+
 	wp_enqueue_script( 'indymedia_customizer_controls', get_stylesheet_directory_uri() . '/js/controls.js', array( 'customize-preview' ), '20180224', true );
 }
 add_action( 'customize_controls_enqueue_scripts', 'indymedia_customizer_controls' );
---
+++ js/customizer.js
@@ -0,0 +1,133 @@
+( function( api ) {
+	api.data = window._wpIndymediaData;
+
+	api.bind( 'ready', function() {
+		api.section.add(
+			new api.Section( 'indymedia_fonts', {
+				title: api.data.l10n.fontsSectionTitle,
+				panel: 'viral_general_settings_panel',
+				priority: 50
+			})
+		);
+
+		api.control.add(
+			new api.Control( 'indymedia_section_tag_font_selector', {
+				label: api.data.l10n.sectionTagsFont,
+				section: 'indymedia_fonts',
+				setting: api( 'indymedia_section_tag_font' ),
+				type: 'select',
+				choices: api.data.fonts
+			})
+		);
+
+		api.control.add(
+			new api.Control( 'indymedia_hero_title_font_selector', {
+				label: api.data.l10n.heroTitleFont,
+				description: api.data.l10n.heroTitleFontDescription,
+				section: 'indymedia_fonts',
+				setting: api( 'indymedia_hero_title_font' ),
+				type: 'select',
+				choices: api.data.fonts
+			})
+		);
+
+		api.control.add(
+			new api.Control( 'indymedia_block_title_font_selector', {
+				label: api.data.l10n.blockTitleFont,
+				description: api.data.l10n.blockTitleFontDescription,
+				section: 'indymedia_fonts',
+				setting: api( 'indymedia_block_title_font' ),
+				type: 'select',
+				choices: api.data.fonts
+			})
+		);
+
+		api.control.add(
+			new api.Control( 'indymedia_post_title_font_selector', {
+				label: api.data.l10n.postTitleFont,
+				description: api.data.l10n.postTitleFontDescription,
+				section: 'indymedia_fonts',
+				setting: api( 'indymedia_post_title_font' ),
+				type: 'select',
+				choices: api.data.fonts
+			})
+		);
+
+		api.control.add(
+			new api.Control( 'indymedia_post_excerpt_font_selector', {
+				label: api.data.l10n.postExcerptFont,
+				description: api.data.l10n.postExcerptFontDescription,
+				section: 'indymedia_fonts',
+				setting: api( 'indymedia_post_excerpt_font' ),
+				type: 'select',
+				choices: api.data.fonts
+			})
+		);
+
+		api.control.add(
+			new api.Control( 'indymedia_widget_title_font_selector', {
+				label: api.data.l10n.widgetTitleFont,
+				description: api.data.l10n.widgetTitleFontDescription,
+				section: 'indymedia_fonts',
+				setting: api( 'indymedia_widget_title_font' ),
+				type: 'select',
+				choices: api.data.fonts
+			})
+		);
+
+		api.control.add(
+			new api.Control( 'indymedia_entry_title_font_selector', {
+				label: api.data.l10n.entryTitleFont,
+				description: api.data.l10n.entryTitleFontDescription,
+				section: 'indymedia_fonts',
+				setting: api( 'indymedia_entry_title_font' ),
+				type: 'select',
+				choices: api.data.fonts
+			})
+		);
+
+		api.control.add(
+			new api.Control( 'indymedia_entry_content_font_selector', {
+				label: api.data.l10n.entryContentFont,
+				description: api.data.l10n.entryContentFontDescription,
+				section: 'indymedia_fonts',
+				setting: api( 'indymedia_entry_content_font' ),
+				type: 'select',
+				choices: api.data.fonts
+			})
+		);
+
+		api.control.add(
+			new api.Control( 'indymedia_top_header_font_selector', {
+				label: api.data.l10n.topHeaderFont,
+				description: api.data.l10n.topHeaderFontDescription,
+				section: 'indymedia_fonts',
+				setting: api( 'indymedia_top_header_font' ),
+				type: 'select',
+				choices: api.data.fonts
+			})
+		);
+
+		api.control.add(
+			new api.Control( 'indymedia_main_navigation_font_selector', {
+				label: api.data.l10n.mainNavigationFont,
+				description: api.data.l10n.mainNavigationFontDescription,
+				section: 'indymedia_fonts',
+				setting: api( 'indymedia_main_navigation_font' ),
+				type: 'select',
+				choices: api.data.fonts
+			})
+		);
+
+		api.control.add(
+			new api.Control( 'indymedia_ticker_font_selector', {
+				label: api.data.l10n.tickerFont,
+				description: api.data.l10n.tickerFontDescription,
+				section: 'indymedia_fonts',
+				setting: api( 'indymedia_ticker_font' ),
+				type: 'select',
+				choices: api.data.fonts
+			})
+		);
+	});
+}( wp.customize ) );
